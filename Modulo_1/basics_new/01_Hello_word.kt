b5317e6ec2015aacbac130c4831181003b637fc5
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-07 20:06:24 -0500

    Clase_4_7_2026

---
+++ Modulo_1/basics_new/01_Hello_word.kt
@@ -0,0 +1,20 @@
+// Declaracion Variables 
+fun main(){
+    //Val Inmutable
+    val nombre= "Ana"
+    val edad: Init = 28
+    //Var mutable
+    var contador = 0
+    contador = contador + 1
+
+    println("$nombre tiene $edad años")
+
+    //Tipos de datos
+    //Numeros Enteros
+    val numero1: Byte = 127
+    val numero2: Short = 32_767
+    val numero3: Int = 12
+    val numero4: Long = 2_222_222_333_222_233
+    -
+
+}
